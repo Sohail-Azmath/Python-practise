b6c79ecc05ccddb9a60abbe3ca827c8a0ed194ec
Sohail-Azmath <mdsohailazmath@gmail.com>
2025-05-13 23:47:40 +0530

    refined folder structured

---
+++ KwIden.py
@@ -0,0 +1,29 @@
+import keyword
+
+print(keyword.kwlist)
+
+
+# Identifiers
+name = 'Mohammed'
+print(name)
+
+_ = 'Sohail Azmath'
+print(_)
+
+# $ = 'Sohail Azmath'
+# print($)
+#           $ = 'Sohail Azmath'
+#     ^
+# SyntaxError: invalid syntax
+
+# Input function
+first = int(input("Enter your place:"))
+second = int (input("Enter your name:"))
+print(first)
+print(second)
+print(first + second)
+
+print(type(first))
+print(type(second))
+
+4+5
---
+++ day1.py
@@ -0,0 +1,30 @@
+name = 1
+print(name)
+
+name = 'Hello world'
+print(name)
+
+name = 3.14
+print(name)
+
+name = True
+print(name)
+
+
+
+#Special declaration techniques
+a = 1; b = 2; c = 3
+print(a)
+print(b)
+print(c)
+
+#Multiple assignment
+a, b, c = 4, 5, 6
+print(a)
+print(b)
+print(c)
+
+a = b = c = 7
+print("value a:",a)
+print("value b:",b)
+print("value c:",c)
