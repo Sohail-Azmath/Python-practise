a7f327a05c0a35f25cc394a8cf98e6b5e167321e
Sohail-Azmath <mdsohailazmath@gmail.com>
2025-05-16 18:45:10 +0530

    Nested if

--- DecisionControl.py
+++ DecisionControl.py
@@ -6,5 +6,12 @@ password = input("Enter your password:")
 
 if email == "campusx@gmail.com" and password == "1234":
     print("Login successful")
+elif email == "campusx@gmail.com" and password != "1234":
+    print("Incorrect password")
+    password = input("Enter your password again:")
+    if password == "1234":
+        print("Finally correct password, Welcome to campusx")
+    else:
+        print("incorrect password")
 else:
     print("Login failed")
