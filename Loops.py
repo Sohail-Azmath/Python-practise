87a32114748089005b9881af57a12b6ddb495ee2
Sohail-Azmath <mdsohailazmath@gmail.com>
2025-05-16 22:59:04 +0530

    Control flow statements

--- Loops.py
+++ Loops.py
@@ -44,3 +44,19 @@ for i in range(1,rows+1):
     for j in range(0,i):
         print("*",end=" ")
     print("")
+
+#break statement
+for i in range(1,11):
+    if i == 5:
+        break
+    print(i)
+print("End of loop")
+
+# continue statement
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(i)
+
+for i in range(1,11):
+    pass
