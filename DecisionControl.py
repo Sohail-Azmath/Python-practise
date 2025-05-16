ec03dd6f3a59016f670406e9c5d39c6baa428920
Sohail-Azmath <mdsohailazmath@gmail.com>
2025-05-16 18:24:32 +0530

    login credential code

---
+++ DecisionControl.py
@@ -0,0 +1,10 @@
+# correct email - campusx@hgmail.com
+# password - 1234
+
+email = input("Enter your email:")
+password = input("Enter your password:")
+
+if email == "campusx@gmail.com" and password == "1234":
+    print("Login successful")
+else:
+    print("Login failed")
