e4da48799506360c70210bd77043607b17d78bee
Sohail-Azmath <mdsohailazmath@gmail.com>
2025-05-17 06:53:47 +0530

    Builtin Functions

---
+++ BuiltinFun.py
@@ -0,0 +1,42 @@
+# print function
+print("hello world")
+
+# input function
+a=input("Press any key to give input")
+
+# type function
+
+print(type(a))
+print(type(3.5))
+
+print(int(5.6))
+print(float(5))
+print(str(5))
+print(bool(0))
+print(list("hello"))
+print(tuple("hello"))
+print(set("hello"))
+
+print("Absolute value of -5:",abs(-5))
+print("Exponential value of 2^3:",pow(2,3))
+print("Maximum value of 5 and 10:",max(5,10))
+print("Minimum value of 5 and 10:",min(5,10))
+
+c = 22/7
+print("original c value before round off:",c)
+print("round off value of c is :",round(c,3))
+
+print("Divmod of two numbers:",divmod(5,2))
+
+print("printing the binary value of a number:",bin(7))
+print("printing the octal value of a number:",oct(10))
+print("printing the hexadecimal value of a number:",hex(10))
+
+a = 10
+print("memory address of 10 is:",id(a))
+
+print("ASCII value of A is:",ord('A'))
+print("length of string :",len("hello_World"))
+
+a = [4,5,6,7,8]
+print("summation of given list is:",sum(a))
